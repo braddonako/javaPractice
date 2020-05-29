45d0118371161d6a48f74c1501fb1961a4e5fbd7
Brad Donakowski <braddonako@gmail.com>
2020-05-28 23:32:30 -0600

    added a person controller

--- demo/src/main/java/com/example/demo/dao/PersonDao.java
+++ demo/src/main/java/com/example/demo/dao/PersonDao.java
@@ -1,8 +1,10 @@
 package com.example.demo.dao;
 
+import com.example.demo.model.Person;
+
 import java.util.UUID;
 
-//The functionality of this API is to hide from the application all the complexities involved 
+//The functionality of this API is to hide from the application all the complexities involved
 // in performing CRUD operations in the underlying storage mechanism.
 // This permits both layers to evolve separately without knowing anything about each other.
 
@@ -10,7 +12,7 @@ public interface PersonDao {
     // this is the contract for anyone that wants to use this interface
 
     int insertPerson(UUID id, Person person);
-    default int addPerson(Person person){
+    default int insertPerson(Person person){
         UUID id = UUID.randomUUID();
         return insertPerson(id, person);
     }
