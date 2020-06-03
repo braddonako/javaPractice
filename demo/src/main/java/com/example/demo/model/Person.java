093a9417eee7924cadee5792de5e077c55d983f1
Brad Donakowski <braddonako@gmail.com>
2020-06-03 14:35:52 -0600

    I cant get valid and NotBlank to work

--- demo/src/main/java/com/example/demo/model/Person.java
+++ demo/src/main/java/com/example/demo/model/Person.java
@@ -2,10 +2,14 @@ package com.example.demo.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+
+
 import java.util.UUID;
+
 
 public class Person {
     private final UUID id;
+
     private final String name;
 
     public Person(@JsonProperty("id") UUID id,
